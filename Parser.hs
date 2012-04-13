d80111a3f1c2fbbc390ac0ba889ef852a75eb10b
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-13 23:19:53 +1000

    cleaned up parser

--- Parser.hs
+++ Parser.hs
@@ -4,24 +4,8 @@ import Syntax
 
 import Text.ParserCombinators.Parsec
 
-run :: Show a => Parser a -> String -> IO ()
-run p input
-        = case (parse p "" input) of
-            Left err -> putStr "parse error at " >> print err
-            Right x  -> print x
 
-as :: Parser a -> (a -> b) -> Parser b
-as = flip fmap
-
-construct :: Parser b -> (a -> a -> a) -> Parser (a -> a -> a)
-construct p c = p >> return c
-
-despace p = do x <- p
-               spaces
-               return x
-
-symbol s   = despace (string s)
-identifier = despace (many1 letter)
+-- Grammar Rules --
 
 term :: Parser [Term]
 term = do spaces
@@ -31,29 +15,69 @@ term = do spaces
 
 atom = identifier `as` Atom <?> "atomic formula"
 
-parens p = do symbol "("
-              inner <- p
-              symbol ")"
-              return inner
-
 atomish =  atom
        <|> parens expr
+
+negation = atomish >>= negs
+
+negs x = do
+  n <- optionMaybe (symbol "^")
+  case n of
+    Just _  -> negs (Not x)
+    Nothing -> return x
+
 
 expr  = expr1 `chainr1` lolly
 expr1 = expr2 `chainr1` add
 expr2 = expr3 `chainr1` mul
 expr3 = expt
 
+
 mul    = tensor <|> par
 tensor = symbol "*" `construct` (:*:)
 par    = symbol "$" `construct` (:$:)
+
+lolly  = symbol "-@" `construct` (:-@:)
+
 
 add    = with <|> plus
 with   = symbol "&" `construct` (:&:)
 plus   = symbol "+" `construct` (:+:)
 
-lolly  = symbol "-@" `construct` (:-@:)
 
-expt     = ofcourse <|> whynot <|> atomish
+expt     = ofcourse <|> whynot <|> negation
 ofcourse = symbol "!" >> expt `as` OfCourse
 whynot   = symbol "?" >> expt `as` WhyNot
+
+
+-- Useful Combinators --
+
+despace p = do x <- p
+               spaces
+               return x
+
+symbol s   = despace (string s)
+identifier = despace (many1 letter)
+
+parens p = do symbol "("
+              inner <- p
+              symbol ")"
+              return inner
+
+
+-- Utility Functions --
+
+run :: Show a => Parser a -> String -> IO ()
+run p input
+        = case (parse p "" input) of
+            Left err -> putStr "parse error at " >> print err
+            Right x  -> print x
+
+as :: Parser a -> (a -> b) -> Parser b
+as = flip fmap
+
+giving :: Parser b -> a -> Parser a
+giving p x = p >> return x
+
+construct :: Parser b -> (a -> a -> a) -> Parser (a -> a -> a)
+construct p c = p >> return c
