169f578f6569fb816f9bce65a103a63c8a3c456b
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-13 23:25:49 +1000

    added pretty printer

--- Syntax.hs
+++ Syntax.hs
@@ -23,4 +23,33 @@ data Term
      | One
      | Zero
 
-  deriving Show
+
+
+showPrecOp prec op d (l, r) = showParen (d > prec) $
+      showsPrec (prec+1) l . showString op . showsPrec (prec+1) r
+
+instance Show Term where
+  showsPrec d (Atom s) = showString s
+
+  showsPrec d (Not t)  = showParen (d > prec) $
+      showsPrec (prec+1) t . showString "^"
+    where prec = 8
+
+  showsPrec d (OfCourse t)  = showParen (d > prec) $
+      showString "!" . showsPrec (prec+1) t
+    where prec = 7
+
+  showsPrec d (WhyNot t)  = showParen (d > prec) $
+      showString "?" . showsPrec (prec+1) t
+    where prec = 7
+
+  showsPrec d (l :*: r)  = showPrecOp 6 "*"  d (l, r)
+  showsPrec d (l :$: r)  = showPrecOp 6 "$"  d (l, r)
+  showsPrec d (l :-@: r) = showPrecOp 2 "  -@  " d (l, r)
+
+  showsPrec d (l :&: r) = showPrecOp 4 " & " d (l, r)
+  showsPrec d (l :+: r) = showPrecOp 4 " + " d (l, r)
+
+  showList []     = id
+  showList (x:[]) = shows x . showString "."
+  showList (x:xs) = shows x . showString ".\n" . showList xs
