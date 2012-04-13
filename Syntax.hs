f68c073908715970d600a6204c9a5fecdea92462
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-13 23:45:07 +1000

    added Printer module

--- Syntax.hs
+++ Syntax.hs
@@ -24,38 +24,3 @@ data Term
      | Zero
 
   deriving Show
-
-showsTermOp prec op d (l, r) = showParen (d > prec) $
-      showsTerm (prec+1) l . showString op . showsTerm (prec+1) r
-
-showTerm :: Term -> String
-showTerm term = showsTerm 0 term ""
-
-showsTerm :: Int -> Term -> ShowS
-showsTerm d (Atom s) = showString s
-
-showsTerm d (Not t)  = showParen (d > prec) $
-    showsTerm (prec+1) t . showString "^"
-  where prec = 8
-
-showsTerm d (OfCourse t)  = showParen (d > prec) $
-    showString "!" . showsTerm (prec+1) t
-  where prec = 7
-
-showsTerm d (WhyNot t)  = showParen (d > prec) $
-    showString "?" . showsTerm (prec+1) t
-  where prec = 7
-
-showsTerm d (l :*: r)  = showsTermOp 6 "*"  d (l, r)
-showsTerm d (l :$: r)  = showsTermOp 6 "$"  d (l, r)
-showsTerm d (l :-@: r) = showsTermOp 2 "  -@  " d (l, r)
-
-showsTerm d (l :&: r) = showsTermOp 4 " & " d (l, r)
-showsTerm d (l :+: r) = showsTermOp 4 " + " d (l, r)
-
-
-showTerms ls = showsTerms ls ""
-
-showsTerms []     = id
-showsTerms (x:[]) = showsTerm 0 x . showString "."
-showsTerms (x:xs) = showsTerm 0 x . showString ".\n" . showsTerms xs
--- linear-logic.hs
+++ linear-logic.hs
@@ -3,5 +3,6 @@ module Main where
 import Bag
 import Syntax
 import Parser
+import Printer
 
 main = getLine >>= run term >> main
