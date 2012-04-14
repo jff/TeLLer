0e1c3b70006a3f7dc8e55803b0feddca3f580496
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-14 17:22:12 +1000

    added test for exponential lattice reduction

--- Logic.hs
+++ Logic.hs
@@ -4,6 +4,8 @@ import Arbitrary
 
 import Syntax
 
+-- Simplify Negations --
+
 -- Negation Involution
 simplify (Not (Not t))    = simplify t
 
@@ -15,8 +17,8 @@ simplify (Not (a :&: b))  = neg a :+:  neg b
 simplify (Not (a :+: b))  = neg a :&:  neg b
 
 -- Exponential Dualities
-simplify (Not (OfCourse a)) = WhyNot   (neg a)
-simplify (Not (WhyNot a))   = OfCourse (neg a)
+simplify (Not (OfCourse a)) = simplify (WhyNot   (Not a))
+simplify (Not (WhyNot a))   = simplify (OfCourse (Not a))
 
 -- Unit Dualities
 simplify (Not One)    = Bottom
@@ -36,8 +38,8 @@ simplify (WhyNot t)   = WhyNot   (simplify t)
 
 simplify t = t
 
-
 neg t = simplify (Not t)
+
 
 
 prop_simplify_idempotent x  =  simplify (simplify x) == simplify x
@@ -52,3 +54,27 @@ prop_simplify_onlyNegativeAtoms x  =  check (simplify x)
         check (OfCourse a) = check a
         check (WhyNot a)   = check a
         check _ = True
+
+prop_simplify_exponentialLattice x  =  check (simplify x)
+  where check (OfCourse (WhyNot (OfCourse a))) = checkNonExp a
+        check (WhyNot   (OfCourse (WhyNot a))) = checkNonExp a
+        check (OfCourse (WhyNot a))   = checkNonExp a
+        check (WhyNot   (OfCourse a)) = checkNonExp a
+        check (OfCourse a) = checkNonExp a
+        check (WhyNot   a) = checkNonExp a
+        check (l :*:  r) = check l && check r
+        check (l :$:  r) = check l && check r
+        check (l :-@: r) = check l && check r
+        check (l :&:  r) = check l && check r
+        check (l :+:  r) = check l && check r
+        check (Not a)    = check a
+        check _ = True
+
+        checkNonExp (OfCourse _) = False
+        checkNonExp (WhyNot   _) = False
+        checkNonExp t = check t
+
+
+-- see:
+-- http://llwiki.ens-lyon.fr/mediawiki/index.php/Sequent_calculus#One-sided_sequent_calculus
+-- http://llwiki.ens-lyon.fr/mediawiki/index.php/Lattice_of_exponential_modalities
