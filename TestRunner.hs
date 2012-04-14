c04110e0f20fa26fa4cd98701b4001b0ed9c1a6c
Andy Kitchen <kitchen.andy@gmail.com>
2012-04-14 23:14:37 +1000

    added names to tests in TestRunner

--- TestRunner.hs
+++ TestRunner.hs
@@ -7,18 +7,22 @@ import Test.QuickCheck
 import Logic
 import Bag
 
-data Test = forall a. (Testable a) => Test a
+data Test = forall a. (Testable a) => Test String a
 
 tests :: [Test]
 tests = [
-    Test prop_simplify_idempotent,
-    Test prop_simplify_onlyNegativeAtoms,
-    Test prop_simplify_inExponentialLattice,
-    Test prop_subbag_reflexive,
-    Test prop_subbag_less,
-    Test prop_subbag_more,
-    Test prop_printParse_ident
+    Test "Simplify -- Idempotent"
+         prop_simplify_idempotent,
+    Test "Simplify -- Only negative atoms"
+         prop_simplify_onlyNegativeAtoms,
+    Test "Simplify -- In Exponential Lattice"
+         prop_simplify_inExponentialLattice,
+    Test "Subbag -- Reflexive" prop_subbag_reflexive,
+    Test "Subbag -- Less" prop_subbag_less,
+    Test "Subbag -- More" prop_subbag_more,
+    Test "Parser -- Print and Parse is Identity" prop_printParse_ident
   ]
 
 main = sequence_ (map runCheck tests)
-     where runCheck (Test t) = quickCheck t
+  where runCheck (Test s t) =
+          putStrLn s >> quickCheckWith (stdArgs {maxSuccess = 10000}) t
